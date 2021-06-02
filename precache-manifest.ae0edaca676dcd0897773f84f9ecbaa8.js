a89b9ca80562e438f6ad9f50ef021461247b6288
ChillyWilly <noreply>
2021-06-02 19:27:44 -0400

    Updates

---
+++ precache-manifest.ae0edaca676dcd0897773f84f9ecbaa8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc81ac1e066ecda9d272ec375484650a",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "11bc75e8968f10d2139e",
+    "url": "/portfolio-react/static/css/main.df3fb930.chunk.css"
+  },
+  {
+    "revision": "fd66c0ff7c93681cf35a",
+    "url": "/portfolio-react/static/js/2.a54187dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-react/static/js/2.a54187dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bc75e8968f10d2139e",
+    "url": "/portfolio-react/static/js/main.81b69daa.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "0b50babf35da7b459d1e7cca55b716f9",
+    "url": "/portfolio-react/static/media/NovidTitleImage.0b50babf.png"
+  },
+  {
+    "revision": "092b2c5db625747328303eee766dde34",
+    "url": "/portfolio-react/static/media/Paul.092b2c5d.jpeg"
+  },
+  {
+    "revision": "ed094fc7f09836fcb277a628c08b9afe",
+    "url": "/portfolio-react/static/media/budgetTracker.ed094fc7.png"
+  },
+  {
+    "revision": "b86c2f78d36d1bef2996648937dbe871",
+    "url": "/portfolio-react/static/media/love-monster.b86c2f78.png"
+  },
+  {
+    "revision": "3c5e14bd1973f479a420510c95611bb7",
+    "url": "/portfolio-react/static/media/noteTaker.3c5e14bd.png"
+  },
+  {
+    "revision": "5e7e23cb6cf9b904535c12da68c2b07a",
+    "url": "/portfolio-react/static/media/pizzaHunt.5e7e23cb.png"
+  },
+  {
+    "revision": "7c260f852750dde9293edebd8799bd17",
+    "url": "/portfolio-react/static/media/run-buddy-img.7c260f85.png"
+  }
+]);
